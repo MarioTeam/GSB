2e15d322aba00a28265c02be32d3cb1c5931a110
wontonmin <Brandon@Wonton>
2014-03-17 15:43:45 +0100

    TEST

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include("vues/v_entete.php") ;
 session_start();
 $pdo = PdoGsb::getPdoGsb();
 $estConnecte = estConnecte();
-//test
+//testFABIEN
 if(!isset($_REQUEST['uc']) || !$estConnecte){
      $_REQUEST['uc'] = 'connexion';
 }	 
