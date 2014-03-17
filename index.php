bb3f399532f6972fff974cb85d18e028392d6ccc
wontonmin <Brandon@Wonton>
2014-03-17 15:06:06 +0100

    Dépot des fichiers

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include("vues/v_entete.php") ;
 session_start();
 $pdo = PdoGsb::getPdoGsb();
 $estConnecte = estConnecte();
-//testaaa
+//testFABIANUS
 if(!isset($_REQUEST['uc']) || !$estConnecte){
      $_REQUEST['uc'] = 'connexion';
 }	 
